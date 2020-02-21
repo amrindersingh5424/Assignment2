948b737c2586f981c3e817268b83a756264ea395
Amrinder Singh <Asingh5424@conestogac.on.ca>
2020-02-21 15:20:09 -0500

    all done

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int side1, side2, side3;
+            Console.WriteLine("1. Enter triangle dimensions");
+            Console.WriteLine("2. Exit");
+
+            int input = int.Parse(Console.ReadLine());
+            if (input == 1) {
+                Console.WriteLine("Enter value of  side 1");
+                side1 = gettingDimensions();
+
+                Console.WriteLine("Enter value of  side 2");
+                side2 = gettingDimensions();
+
+                Console.WriteLine("Enter value of  side 3");
+                side3 = gettingDimensions();
+
+                
+            }
+            else { Environment.Exit(0); }
+            
+        }
+        public static int gettingDimensions() {
+            int dimension = 0;
+            string input = "";
+            do {
+                input = Console.ReadLine();
+            }
+            while (!int.TryParse(input, out dimension) || dimension < 0);
+            return dimension
+        return 
+                }
+    }
+}
---
+++ TriangleSolver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+  public static  class TriangleSolver
+    {
+       public static String answer;
+        public  static String Analyze(int side1, int side2, int side3)
+        {
+            if (((side1 + side2) > side3) && ((side1 + side3) > side2) && ((side2 + side3) > side1))
+            {
+                 if (side1 == side2 || side2 == side3 || side1 == side2)
+                {
+                    answer = "It is isosceles Triangle";
+                }
+                else if (side1 == side2 && side2 == side3 && side3 == side1)
+                {
+                    answer = "It is Equilateral Triangle";
+                }
+                
+                else
+                {
+                    answer = "It is Scalene Triangle";
+                }
+            }
+
+
+            else
+            {
+                answer = "Sorry the sides do not form a triangle";
+            }
+            return answer;
+
+        }
+    }
+}
